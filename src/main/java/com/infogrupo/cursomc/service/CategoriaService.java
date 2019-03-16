2f2697b30b3c4508b23802be46bcbb0eecb658eb
Carlos Lopes <Carlos Lopes@DESKTOP-NQQH900>
2019-03-16 19:08:05 -0300

    Correções: Update em categoria service Correções: Update na classe pagamento

--- src/main/java/com/infogrupo/cursomc/service/CategoriaService.java
+++ src/main/java/com/infogrupo/cursomc/service/CategoriaService.java
@@ -34,9 +34,12 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
 		
-		return categoriaRepository.save(obj);
+		Categoria newObj = find(obj.getId());
+		
+		updateData(newObj, obj);
+		
+		return categoriaRepository.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -65,5 +68,10 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
 	}
 }
