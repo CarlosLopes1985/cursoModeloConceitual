db17d4822960a5f875f172f4564aea810da3b444
Carlos Lopes <Carlos Lopes@DESKTOP-NQQH900>
2019-03-18 22:37:41 -0300

    Enviando email

--- src/main/java/com/infogrupo/cursomc/config/DevConfig.java
+++ src/main/java/com/infogrupo/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.infogrupo.cursomc.service.DBService;
+import com.infogrupo.cursomc.service.EmailService;
+import com.infogrupo.cursomc.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,10 @@ public class DevConfig {
 		dbService.InstantiateTestDatabase();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/infogrupo/cursomc/service/DBService.java
+++ src/main/java/com/infogrupo/cursomc/service/DBService.java
@@ -99,7 +99,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Carlos", "caka19@hotmail.com", "11792993706", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Carlos", "caka19@gmail.com", "11792993706", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("33587928", "981759533"));
 
